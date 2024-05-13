fd3a63ccb402401e02d939d39cd69c5a2f6edcc1
Vaibhav Chauhan <vaibhavchauhanrajput74321@gmail.com>
2024-05-13 14:10:52 +0530

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+        <span className='orange_gradient '>OpenAI GPT-3.5</span>
       </h1>
       <h2 className='desc'>
         Simplify your reading with Summize, an open-source article summarizer
